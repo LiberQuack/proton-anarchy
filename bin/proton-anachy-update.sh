971c1a29c51afe5ccc5c7a2cf6083b8b19597fed
Thiago Martins <thiago.martins@pluto.tv>
2024-12-30 16:16:47 -0400

    Fix update script

--- bin/proton-anachy-update.sh
+++ bin/proton-anachy-update.sh
@@ -2,7 +2,8 @@
 set -e
 
 cd /var/games/proton-anarchy
-git -c safe.directory=$(pwd) pull
+git -c safe.directory=$(pwd) fetch
+git -c safe.directory=$(pwd) checkout origin/main -- bin
 chmod +x /var/games/proton-anarchy/bin/*
 
 echo "Success"
