deebed77f27bfa48f36b07af9e8203a238232566
Thiago Martins <thiago.martins@pluto.tv>
2025-01-07 18:08:43 -0400

    Fix install game script

--- bin/anarchy-install-game.sh
+++ bin/anarchy-install-game.sh
@@ -28,10 +28,10 @@ echo "Creating symbolic link"
 GAME_LINK_DIR="/var/games/$GAME_NAME_SANITIZED"
 ln -sf "$DESTINATION" "$GAME_LINK_DIR"
 
-PROTON_LINK="Z:\\$(echo "$GAME_LINK_DIR" | sed -E 's/\//\\/g')"
+PROTON_LINK="Z:$(echo "$GAME_LINK_DIR" | sed -E 's/\//\\/g')"
 echo "Proton destination is $PROTON_LINK"
 ( echo "$PROTON_LINK" | wl-copy ) || ( echo "$PROTON_LINK" |  xclip -selection clipboard )
-zenity --info --title="Proton destination path utility" --text="$PROTON_LINK copied to your clipboard"  2> /dev/null
+zenity --info --title="Proton destination path utility" --text="$(echo $PROTON_LINK | sed -E 's/\\/\\\\/g') copied to your clipboard"  2> /dev/null
 ./anarchy-run.sh "$EXECUTABLE" || zenity --question --title="Confirmation" --text="Could not detect successful installation, do you want to continue with steam integration?"  2> /dev/null
 
 echo "Preparing to pick game executable file"
