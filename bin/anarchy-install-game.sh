cd811730cf8a404a5289ea6c8f3ae6c61d0353e6
Thiago Martins <thiago.martins@pluto.tv>
2025-01-07 17:45:31 -0400

    Fix install game script

--- bin/anarchy-install-game.sh
+++ bin/anarchy-install-game.sh
@@ -14,28 +14,28 @@ GAME_NAME=$2
 . anarchy-setup-variables.sh
 
 if ! [ -e "$EXECUTABLE" ]; then
-    EXECUTABLE=$(zenity --file-selection --title="Select the installer" --filename="$(pwd)" &> /dev/null)
+    EXECUTABLE=$(zenity --file-selection --title="Select the installer" --filename="$(pwd)" 2> /dev/null)
     echo "You selected: $EXECUTABLE"
 fi
 
 echo "Preparing to install non steam game $EXECUTABLE"
-GAME_NAME="${GAME_NAME:-$(zenity --entry --title="Game Name" --entry-text "$(basename "$(dirname "$EXECUTABLE")")"  &> /dev/null)}"
+GAME_NAME="${GAME_NAME:-$(zenity --entry --title="Game Name" --entry-text "$(basename "$(dirname "$EXECUTABLE")")"  2> /dev/null)}"
 GAME_NAME_SANITIZED=$(echo "$GAME_NAME" | tr '[:upper:]' '[:lower:]' | sed -E 's/[[:space:]]+/-/g; s/-+/-/g')
-DESTINATION="$(zenity --entry --title="Destination" --entry-text "$(dirname $ORIGINAL_PATH)/$GAME_NAME_SANITIZED"  &> /dev/null)"
+DESTINATION="$(zenity --entry --title="Destination" --entry-text "$(dirname $ORIGINAL_PATH)/$GAME_NAME_SANITIZED"  2> /dev/null)"
 mkdir -p "$DESTINATION"
 
 echo "Creating symbolic link"
 GAME_LINK_DIR="/var/games/$GAME_NAME_SANITIZED"
 ln -sf "$DESTINATION" "$GAME_LINK_DIR"
 
-PROTON_LINK="Z:\\$(echo "$GAME_LINK_DIR" | sed -E 's/\//\\\\/g')"
+PROTON_LINK="Z:\\$(echo "$GAME_LINK_DIR" | sed -E 's/\//\\/g')"
 echo "Proton destination is $PROTON_LINK"
 ( echo "$PROTON_LINK" | wl-copy ) || ( echo "$PROTON_LINK" |  xclip -selection clipboard )
-zenity --info --title="Proton destination path utility" --text="$PROTON_LINK copied to your clipboard"  &> /dev/null
-./anarchy-run.sh "$EXECUTABLE" || zenity --question --title="Confirmation" --text="Could not detect successful installation, do you want to continue with steam integration?"  &> /dev/null
+zenity --info --title="Proton destination path utility" --text="$PROTON_LINK copied to your clipboard"  2> /dev/null
+./anarchy-run.sh "$EXECUTABLE" || zenity --question --title="Confirmation" --text="Could not detect successful installation, do you want to continue with steam integration?"  2> /dev/null
 
 echo "Preparing to pick game executable file"
-EXECUTABLE="$(zenity --file-selection --title="Select the game executable" --filename="$GAME_LINK_DIR"  &> /dev/null)"
+EXECUTABLE="$(zenity --file-selection --title="Select the game executable" --filename="$GAME_LINK_DIR"  2> /dev/null)"
 
 echo "Preparing to create .desktop file into $GAME_LINK_DIR"
 DESKTOP_FILE="$GAME_LINK_DIR/${GAME_NAME_SANITIZED}.desktop"
@@ -63,5 +63,5 @@ steamtinkerlaunch addnonsteamgame --use-steamgriddbb --auto-artwork \
     --steamgriddb-game-name="$GAME_NAME" \
     -lo="STEAM_COMPAT_MOUNTS=\"\$STEAM_COMPAT_MOUNTS:$SCRIPT_DIR:$GAME_LINK_DIR:$DESTINATION\" gtk-launch %command%"
 
-( zenity --info --text="$GAME added! Please restart steam"  &> /dev/null & )
+( zenity --info --text="$GAME added! Please restart steam"  2> /dev/null & )
 echo "$GAME added to steam!!!"
